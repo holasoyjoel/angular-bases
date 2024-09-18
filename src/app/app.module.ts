190fbadc0ca3c7be1937b1ed7bac115b052b1dfb
Correa Joel <joelcorrea.alejandro123@gmail.com>
2024-09-18 13:17:52 -0300

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+// Modules
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
